604515f49fd84b06b8255e83b5284e573944c7b7
legionscript <legionscript@legionscript.com>
2021-06-24 15:57:11 -0400

    2. Model Relationships

--- music/catalog/admin.py
+++ music/catalog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Artist
+from .models import Artist, Album
 
 admin.site.register(Artist)
+admin.site.register(Album)
--- music/catalog/models.py
+++ music/catalog/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 class Artist(models.Model):
 	name = models.CharField(max_length=100)
-	email = models.EmailField(unique=True)
+	email = models.EmailField(blank=True, null=True)
 	bio = models.TextField(null=True, blank=True, help_text='Give some info about the artist')
-	song_total = models.IntegerField()
-	choices = models.TextField(choices=[('1', 'Choice 1'), ('2', 'Choice 2')])
+	song_total = models.IntegerField(blank=True, null=True)
+	choices = models.TextField(choices=[('1', 'Choice 1'), ('2', 'Choice 2')], blank=True, null=True)
 	favorite = models.BooleanField(default=False)
 	last_modified = models.DateTimeField(auto_now=True)
 	created = models.DateTimeField(auto_now_add=True)
-	profile_picture = models.ImageField(upload_to='uploads')
-	download = models.FileField(upload_to='uploads')
+	profile_picture = models.ImageField(upload_to='uploads', blank=True, null=True)
+	download = models.FileField(upload_to='uploads', blank=True, null=True)
+
+class Album(models.Model):
+	title = models.CharField(max_length=100)
+	released_on = models.DateTimeField(blank=True, null=True)
+	artists = models.ManyToManyField('Artist')
+
+class Song(models.Model):
+	title = models.CharField(max_length=100)
+	album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+class CatalogItem(models.Model):
+	name = models.CharField(max_length=100)
+	item = models.OneToOneField('Album', on_delete=models.CASCADE)

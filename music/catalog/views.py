4b1521e919d88048af3a9581e5d95cb303528a22
legionscript <legionscript@legionscript.com>
2021-11-25 12:36:50 -0500

    Function Views

--- music/catalog/views.py
+++ music/catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from .models import Artist
 
-# Create your views here.
+# def current_datetime(request):
+# 	now = datetime.datetime.now()
+# 	# html = "<html><body><p>It is now %s.</p></body></html>" % now
+# 	# return HttpResponse(html)
+
+# 	context = {
+# 		'now': now
+# 	}
+
+# 	return render(request, 'catalog/index.html', context)
+
+def index(request):
+	artist = Artist.objects.get(pk=1)
+
+	context = {
+		'artist': artist
+	}
+
+	return render(request, 'catalog/index.html', context)
+
+def sample_form(request):
+	if request.method == 'POST':
+		first_name = request.POST.get('firstName')
+		print (first_name)
+		return render(request, 'catalog/sample_form.html')
+	else:
+		print('get')
+		return render(request, 'catalog/sample_form.html')
+
--- music/music/urls.py
+++ music/music/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from catalog.views import index, sample_form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('sample-form/', sample_form)
 ]
